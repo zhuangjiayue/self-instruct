f80fc6fbb0b9a8bd72f490589cf4edd5d424a973
zhuang_jy <710499547@qq.com>
2023-07-03 17:26:39 +0800

    添加例子

---
+++ qa_generation/API_KEY.py
@@ -0,0 +1,5 @@
+import os
+os.environ["SERPAPI_API_KEY"] = "5e6805f119e1bcd65d61825c74d3db73feeaa4458db91bb8467fe69523fe1cbb"
+os.environ["OPENAI_API_KEY"] = "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"
+os.environ["http_proxy"] = "http://127.0.0.1:10900"
+os.environ["https_proxy"] = "http://127.0.0.1:10900"
